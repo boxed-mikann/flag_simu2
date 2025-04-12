82eecffd2e1804194f7c699412424b0634b4cc22
boxed-mikann <u085421i@ecs.osaka-u.ac.jp>
2025-04-12 18:19:02 +0900

    githubpagesデプロイ用

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/flag_simu2/', // GitHubリポジトリ名を設定
   server: {
     host: '0.0.0.0', // コンテナ内部からのアクセスを許可
     port: 5173,
